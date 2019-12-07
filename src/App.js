60d142c7882bffa9bca5cbf38ed82f53b70ddfe4
Brian <briandsalemi@gmail.com>
2019-12-07 15:58:56 -0500

    modified App.js to be class component as opposed to functional, imported React into and exported components Book, BooksContainer, SearchBar, and SideBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return 
+export default class App extends React.Component{
+  render(){
+    return
+  }
 }
 
-export default App;
+
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-export default class Book extends React.Component{
+
+export default class SideBar extends React.Component{
     render(){
         return
     }
