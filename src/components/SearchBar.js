4aee01dc42383c55be5978180cb8928671978a8e
Brian <briandsalemi@gmail.com>
2019-12-12 12:06:44 -0500

    added state for SearchBar component that captures the current state of search. added an onChange event to the input field that updates the state of the search with every key entered

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 export default class SearchBar extends React.Component{
+
+    state = {
+        search: ""
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            search: event.target.value
+        })
+        console.log(this.state)
+    }
+
+
     render(){
         return(
             <div id="search-bar-container">
-                <form id="search-bar">
-                    <input type="text" name="search-bar"/>
+                <form id="search-bar" onSubmit={this.handleOnSubmit}>
+                    <input type="text" name="search-bar" onChange={this.handleOnChange}/>
                     <button>Submit</button>
                 </form>
             </div>
