0718e396875b3c7671cb5ecd7c63f89e46cc4416
Brian <briandsalemi@gmail.com>
2019-12-12 09:39:39 -0500

    created form/input for search bar in SearchBar component, added a Header component file and imported it in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './components/SearchBar';
 import SideBar from './components/SideBar';
 import BooksContainer from './containers/BooksContainer'
+import Header from './components/Header';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ export default class App extends React.Component{
   render(){
     return(
       <div id="app">
+        <Header/>
         <SearchBar/>
         <SideBar/>
         <BooksContainer/>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,10 @@ export default class SearchBar extends React.Component{
     render(){
         return(
             <div id="searchBar">
-
+                <form>
+                    <input type="text" name="search-bar"/>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
