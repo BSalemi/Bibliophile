03de20a0e405bae4a6f7ea82efc5696636aa676b
Brian <briandsalemi@gmail.com>
2019-12-20 18:47:29 -0500

    added div for book-card in generateBooks() and added CSS to books-container, flexbox, and added height and width for each book card

--- src/containers/BooksContainer.js
+++ src/containers/BooksContainer.js
@@ -6,7 +6,7 @@ export default class BooksContainer extends React.Component{
 
     generateBooks = () => {
         const books = this.props.results.map((bookObj, key) => {
-            return <Book index={bookObj.id} title={bookObj.volumeInfo.title} genre={bookObj.volumeInfo.categories} image={'imageLinks' in bookObj.volumeInfo ? bookObj.volumeInfo.imageLinks.thumbnail : null} link={bookObj.volumeInfo.infoLink} author={'authors' in bookObj.volumeInfo ? bookObj.volumeInfo.authors : null}/>
+            return <div id="book-card"><Book index={bookObj.id} title={bookObj.volumeInfo.title} genre={bookObj.volumeInfo.categories} image={'imageLinks' in bookObj.volumeInfo ? bookObj.volumeInfo.imageLinks.thumbnail : null} link={bookObj.volumeInfo.infoLink} author={'authors' in bookObj.volumeInfo ? bookObj.volumeInfo.authors : null}/></div>
         })
         return books
     }
@@ -15,7 +15,6 @@ export default class BooksContainer extends React.Component{
         console.log(this.props, "props in books container")
         return(
             <div id="books-container">
-               {this.props.results.length > 0 && <h2 id="books-list">Books List:</h2>}
                {this.generateBooks()}
             </div>
         )
